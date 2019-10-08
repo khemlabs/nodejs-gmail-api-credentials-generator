a73b30ed07877eb801bd70d77772038b9ce48efa
Pablo Gambetta <gambeita@gmail.com>
2019-10-07 21:01:20 -0300

    writeFile error solved

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
-var fs = require("fs");
-var readline = require("readline");
-var google = require("googleapis");
-var googleAuth = require("google-auth-library");
+var fs = require('fs');
+var readline = require('readline');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
 
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/gmail-nodejs-quickstart.json
-var SCOPES = process.env.SCOPE
-  ? process.env.CLIENT_SECRET_PATH.split(",")
-  : ["https://mail.google.com/"];
-var TOKEN_DIR =
-  (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) +
-  "/.credentials/";
-var TOKEN_PATH = TOKEN_DIR + "gmail-nodejs-quickstart.json";
+var SCOPES = process.env.SCOPE ? process.env.CLIENT_SECRET_PATH.split(',') : ['https://mail.google.com/'];
+var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) + '/.credentials/';
+var TOKEN_PATH = TOKEN_DIR + 'gmail-nodejs-quickstart.json';
 
 // Load client secrets from a local file.
-fs.readFile(
-  process.env.CLIENT_SECRET_PATH || "client_secret.json",
-  function processClientSecrets(err, content) {
-    if (err) {
-      console.log("Error loading client secret file: " + err);
-      return;
-    }
-    // Authorize a client with the loaded credentials, then call the
-    // Gmail API.
-    authorize(JSON.parse(content), listLabels);
-  }
-);
+fs.readFile(process.env.CLIENT_SECRET_PATH || 'client_secret.json', function processClientSecrets(err, content) {
+	if (err) {
+		console.log('Error loading client secret file: ' + err);
+		return;
+	}
+	// Authorize a client with the loaded credentials, then call the
+	// Gmail API.
+	authorize(JSON.parse(content), listLabels);
+});
 
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
@@ -35,29 +28,29 @@ fs.readFile(
  * @param {function} callback The callback to call with the authorized client.
  */
 function authorize(credentials, callback) {
-  var clientSecret = credentials.installed.client_secret;
-  var clientId = credentials.installed.client_id;
-  var redirectUrl = credentials.installed.redirect_uris[0];
-  var auth = new googleAuth();
-  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+	var clientSecret = credentials.installed.client_secret;
+	var clientId = credentials.installed.client_id;
+	var redirectUrl = credentials.installed.redirect_uris[0];
+	var auth = new googleAuth();
+	var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
 
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, function(err, token) {
-    if (err) {
-      getNewToken(oauth2Client, callback);
-    } else {
-      var date = new Date().toISOString();
-      var NEW_PATH = TOKEN_PATH + '.' + date + '.json';
-      console.log('Old credentials move to ' + NEW_PATH);
-      fs.rename(TOKEN_PATH, NEW_PATH, function(err){
-        if(err){
-          console.log(err);
-        } else {
-          getNewToken(oauth2Client, callback);
-        }
-      });
-    }
-  });
+	// Check if we have previously stored a token.
+	fs.readFile(TOKEN_PATH, function(err, token) {
+		if (err) {
+			getNewToken(oauth2Client, callback);
+		} else {
+			var date = new Date().toISOString();
+			var NEW_PATH = TOKEN_PATH + '.' + date + '.json';
+			console.log('Old credentials move to ' + NEW_PATH);
+			fs.rename(TOKEN_PATH, NEW_PATH, function(err) {
+				if (err) {
+					console.log(err);
+				} else {
+					getNewToken(oauth2Client, callback);
+				}
+			});
+		}
+	});
 }
 
 /**
@@ -69,27 +62,27 @@ function authorize(credentials, callback) {
  *     client.
  */
 function getNewToken(oauth2Client, callback) {
-  var authUrl = oauth2Client.generateAuthUrl({
-    access_type: "offline",
-    scope: SCOPES
-  });
-  console.log("Authorize this app by visiting this url: ", authUrl);
-  var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  rl.question("Enter the code from that page here: ", function(code) {
-    rl.close();
-    oauth2Client.getToken(code, function(err, token) {
-      if (err) {
-        console.log("Error while trying to retrieve access token", err);
-        return;
-      }
-      oauth2Client.credentials = token;
-      storeToken(token);
-      callback(oauth2Client);
-    });
-  });
+	var authUrl = oauth2Client.generateAuthUrl({
+		access_type: 'offline',
+		scope: SCOPES
+	});
+	console.log('Authorize this app by visiting this url: ', authUrl);
+	var rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
+	});
+	rl.question('Enter the code from that page here: ', function(code) {
+		rl.close();
+		oauth2Client.getToken(code, function(err, token) {
+			if (err) {
+				console.log('Error while trying to retrieve access token', err);
+				return;
+			}
+			oauth2Client.credentials = token;
+			storeToken(token);
+			callback(oauth2Client);
+		});
+	});
 }
 
 /**
@@ -98,15 +91,16 @@ function getNewToken(oauth2Client, callback) {
  * @param {Object} token The token to store to disk.
  */
 function storeToken(token) {
-  try {
-    fs.mkdirSync(TOKEN_DIR);
-  } catch (err) {
-    if (err.code != "EEXIST") {
-      throw err;
-    }
-  }
-  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
-  console.log("Token stored to " + TOKEN_PATH);
+	try {
+		fs.mkdirSync(TOKEN_DIR);
+	} catch (err) {
+		if (err.code != 'EEXIST') {
+			throw err;
+		}
+	}
+	fs.writeFile(TOKEN_PATH, JSON.stringify(token), () => {
+		console.log('Token stored to ' + TOKEN_PATH);
+	});
 }
 
 /**
@@ -115,27 +109,27 @@ function storeToken(token) {
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
 function listLabels(auth) {
-  var gmail = google.gmail("v1");
-  gmail.users.labels.list(
-    {
-      auth: auth,
-      userId: "me"
-    },
-    function(err, response) {
-      if (err) {
-        console.log("The API returned an error: " + err);
-        return;
-      }
-      var labels = response.labels;
-      if (labels.length == 0) {
-        console.log("No labels found.");
-      } else {
-        console.log("Labels:");
-        for (var i = 0; i < labels.length; i++) {
-          var label = labels[i];
-          console.log("- %s", label.name);
-        }
-      }
-    }
-  );
+	var gmail = google.gmail('v1');
+	gmail.users.labels.list(
+		{
+			auth: auth,
+			userId: 'me'
+		},
+		function(err, response) {
+			if (err) {
+				console.log('The API returned an error: ' + err);
+				return;
+			}
+			var labels = response.labels;
+			if (labels.length == 0) {
+				console.log('No labels found.');
+			} else {
+				console.log('Labels:');
+				for (var i = 0; i < labels.length; i++) {
+					var label = labels[i];
+					console.log('- %s', label.name);
+				}
+			}
+		}
+	);
 }
